62fd90d5c3b72376ab41ab5306d941d040923bba
RODRIGO BONINI GUERREIRO <rodrigo.bguerreiro@senacsp.edu.br>
2023-02-15 11:28:41 -0300

    add uma nova variavel

--- Program.cs
+++ Program.cs
@@ -10,10 +10,15 @@ namespace Exercicios15_02
     {
         static void Main(string[] args)
         {
+
             double n1 = 0;
+            double n2 = 0;
             Console.WriteLine("digite um numero: ");
             n1 = Convert.ToDouble(Console.ReadLine());
-            // git status esta querendo dizer que eu fiz uma alteração no codigo e não  digitei o git commit //
+            Console.WriteLine("Digite o segundo numero");
+            n2 = Convert.ToDouble(Console.ReadLine());
+     // git status esta querendo dizer que eu fiz uma alteração no codigo e não  digitei o git commit
+     // git log ele mostra as alterações que eu realizei em meu codigo data/hora e o codigo do commit
         }
     }
 }
