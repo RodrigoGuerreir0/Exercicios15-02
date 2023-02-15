126940ce1e476d072cdbc40f60a09a673cbbcd53
rodrigo.bguerreiro <rodrigo.bguerreiro@AME0635290W10-1>
2023-02-15 11:09:55 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios15_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1 = 0;
+            Console.WriteLine("digite um numero: ");
+            n1 = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+}
